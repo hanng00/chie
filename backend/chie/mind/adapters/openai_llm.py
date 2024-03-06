7da3c6ab5a608afaec645fa7a1ddb44109f62c0f
Hannes Gustafsson <hannes.gustafsson@burtcorp.com>
2024-03-06 09:25:05 +0100

    Add the Application logic

---
+++ backend/chie/mind/adapters/openai_llm.py
@@ -0,0 +1,20 @@
+from chie.mind.domain.ports.llm import LLMPort
+from openai import AsyncOpenAI
+
+
+class OpenAILLM(LLMPort):
+    def __init__(self, openai: AsyncOpenAI):
+        self.openai = openai
+
+    async def completion(self, instruction: str, prompt: str) -> str:
+        messages = [
+            {"role": "system", "content": instruction},
+            {"role": "user", "content": prompt},
+        ]
+
+        completion = await self.openai.chat.completions.create(
+            messages=messages,
+            model="gpt-3.5-turbo",
+        )
+
+        return completion.choices[0].message.content
