083e54f7c941044a197260c0ea4a18674f893ff4
Hannes Gustafsson <hannes.gustafsson@burtcorp.com>
2024-03-19 11:16:15 +0100

    Refactor Api Entrypoint

--- backend/chie/main.py
+++ backend/chie/main.py
@@ -8,29 +8,41 @@ from dotenv import load_dotenv
 from chie.modules.mind.controllers.mind_routes import mind_router
 from chie.modules.user.controllers.user_routes import user_router
 
-logger = get_logger(__name__)
 
-app = FastAPI()
+def setup_fastapi_app():
+    app = FastAPI()
 
-add_cors_middleware(app)
+    add_cors_middleware(app)
 
-app.include_router(mind_router, prefix="/mind", tags=["mind"])
-app.include_router(user_router, prefix="/user", tags=["user"])
+    app.include_router(mind_router, prefix="/mind", tags=["mind"])
+    app.include_router(user_router, prefix="/user", tags=["user"])
+
+    return app
 
 
-def main():
+def get_uvicorn_config():
     is_development = os.getenv("APP_ENV", "development").lower() != "production"
     if is_development:
         logger.info("Running in DEVELOPMENT mode")
         load_dotenv()
 
+    return {
+        "host": os.getenv("HOST", "0.0.0.0"),
+        "port": int(os.getenv("PORT", 8000)),
+        "reload": is_development,
+        "workers": 6,
+    }
+
+
+logger = get_logger(__name__)
+app = setup_fastapi_app()
+
+
+def main():
     uvicorn_app = f"chie.main:app"
+    uvicorn_config = get_uvicorn_config()
 
-    host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", 8000))
-    reload = is_development
-
-    uvicorn.run(uvicorn_app, host=host, port=port, reload=reload)
+    uvicorn.run(uvicorn_app, **uvicorn_config)
 
 
 if __name__ == "__main__":
